a2595f971df7a397a4e7c2521d0c0eafcbd27ee3
Boram Lee <boramlee340@gmail.com>
2022-07-06 21:32:36 +0100

    add database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYUFVWPb182Rgp8o9JNZhv1ZwMd5zAkWE",
+  authDomain: "artyourstory.firebaseapp.com",
+  projectId: "artyourstory",
+  storageBucket: "artyourstory.appspot.com",
+  messagingSenderId: "47869260600",
+  appId: "1:47869260600:web:4ba7575d708bc7cfa487cd",
+  databaseURL: "https://artyourstory-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+function writeUserData(userId, name, email, imageUrl) {
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl
+  });
+}
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const db = getDatabase();
+    const usersRef = ref(db, 'users/');
+    onValue(usersRef, (snapshot) => {
+      const data = snapshot.val();
+      setUsers(Object.values(data))
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Dorothy, Saki and Jack! Can you see this?</p>
+
+        {
+          users.map(user => {
+            return <div key={user.username}>
+              {user.username}
+            </div>
+          })
+        }
+
+        <button onClick={() => {
+          writeUserData(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), "borama@borama.bor", null)
+        }}>
+          Click me
+        </button>
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { initializeApp } from "firebase/app";
+
 
 import App from './App';
 
 import './index.css';
 
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBYUFVWPb182Rgp8o9JNZhv1ZwMd5zAkWE",
-  authDomain: "artyourstory.firebaseapp.com",
-  projectId: "artyourstory",
-  storageBucket: "artyourstory.appspot.com",
-  messagingSenderId: "47869260600",
-  appId: "1:47869260600:web:4ba7575d708bc7cfa487cd"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
