42fe224386b75609b67b14f6cdcbaac918c235c3
Boram Lee <boramlee340@gmail.com>
2022-07-22 09:44:36 +0100

    add text componnt

--- src/Text.js
+++ src/Text.js
@@ -1,15 +1,22 @@
+import classNames from "classnames";
+
 import "./Text.css";
 
-export function Text({ children, size }) {
-  const classNames = ["text"];
-
-  if (size === "s") {
-    classNames.push("text-s");
-  } else if (size === "m") {
-    classNames.push("text-m");
-  } else if (size === "l") {
-    classNames.push("text-l");
-  }
-
-  return <span className={classNames.join(" ")}>{children}</span>;
+export function Text({ children, size, bold, color }) {
+  return (
+    <span
+      className={classNames("text", {
+        "text-s": size === "s",
+        "text-m": size === "m",
+        "text-l": size === "l",
+        "text-bold": bold === true,
+        "text-color-primary": color === "primary" || color == null,
+        "text-color-secondary": color === "secondary",
+        "text-color-subtle": color === "subtle",
+        "text-color-light": color === "light",
+      })}
+    >
+      {children}
+    </span>
+  );
 }
