0323a9f5e05b76edad5b9c2517be11e2aaadd159
Saki Unno <unx2o.sk@gmail.com>
2022-07-29 18:09:49 +0900

    showpage

--- src/ShowPage.js
+++ src/ShowPage.js
@@ -18,7 +18,7 @@ return (
               <figure className="image">
                 <img
                   src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                  alt="cute dog"
+                  alt="Shiba Inu"
                 />
               </figure>
             </div>
